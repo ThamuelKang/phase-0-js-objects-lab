dc8ee2b12bc8030694cbddecb16247b1869aca27
Samuel Kang <samuel.june.kang@gmail.com>
2025-04-12 21:06:02 -0700

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 //Write your code here
 
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "Javascript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
+
+
+function logAttendeeName(name) {
+  attendee.name = name
+}
+
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice)
+}
+
+function updateTicketType(newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+
+function removeEventProperty() {
+  delete attendee.event 
+}
+
+function addCheckedInProperty() {
+  attendee.addCheckedInProperty = true
+}
+
+logAttendeeName("samuel")
+console.log(attendee.name)
+
+logTicketPrice(attendee)
+
+updateTicketType("VVVVVIP")
+console.log(attendee.ticketType)
+
+removeEventProperty()
+addCheckedInProperty()
+
+console.log(attendee)
+
+
 
 
 //Needed for the tests to work. Don't modify
